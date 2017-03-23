00afbc48833e0c9dfc2c4223f3e37317bdfe2e97
chishui <chishui2@gmail.com>
2017-03-22 21:55:54 -0700

    update gif

--- leetcode/leetcode.py
+++ leetcode/leetcode.py
@@ -155,10 +155,13 @@ class Leetcode(object):
         elif data['state'] == 'STARTED':
             return (2,)
         elif data['state'] == 'SUCCESS':
-            if data['run_success']:
-                return (0, data['total_correct'], data['total_testcases'], data['status_runtime'])
+            if 'run_success' in data:
+                if data['run_success']:
+                    return (0, data['total_correct'], data['total_testcases'], data['status_runtime'])
+                else:
+                    return (-1, data['compile_error'])
             else:
-                return (-1, data['compile_error'])
+                return (-1, 'Unknow error!')
 
 
 def preprocess_bs(bs):
