4c64f132cd7ef98e94789bc761519181d4a62441
chishui <chishui2@gmail.com>
2017-04-29 09:00:56 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = ['urwid', 'requests', 'bs4', 'lxml']
 
 setup(
     name = "terminal-leetcode",
-    version = "0.0.10",
+    version = "0.0.11",
     author = "Liyun Xiu",
     author_email = "chishui2@gmail.com",
     description = "A terminal based leetcode website viewer",
